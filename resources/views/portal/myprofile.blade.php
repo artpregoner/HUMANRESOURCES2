b68b2bdcbbf4b9557eceff497fa7b8b85fb94099
artpregoner <ajcoderpregoner34@gmail.com>
2024-12-15 23:47:27 +0800

    portal: myprofile, content

--- resources/views/portal/myprofile.blade.php
+++ resources/views/portal/myprofile.blade.php
@@ -4,4 +4,21 @@
 @section('active-header', 'My Profile')
 
 @section('content')
+<div class="row">
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <div class="card">
+            <div class="card-header">
+                <h5 class="email-title">Portal Profile</h5>
+            </div>
+            <div class="card-body">
+                <div class="user-avatar text-center d-block">
+                    <img src="{{asset('template/assets/images/avatar-1.jpg')}}" alt="User Avatar" class="rounded-circle user-avatar-xxl">
+                </div>
+            </div>
+            <div class="card-footer d-flex text-muted">
+                Click on the image above, or drag and drop a new one on top, in order to update the profile picture for your account ( 128 x 128px size recommended. Limit 5MB. ).
+            </div>
+        </div>
+    </div>
+</div>
 @endsection

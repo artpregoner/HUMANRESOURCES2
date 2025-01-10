fc64d4e0dca005cd7fccd9143f403d69f4d6026a
artpregoner <ajcoderpregoner34@gmail.com>
2025-01-10 22:48:10 +0800

    claims index view

---
+++ resources/views/hr2/claims/index.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.app')
+@section('title')Claims & Reimbursement @endsection
+@section('header')Claims & Reimbursement @endsection <!--pageheader-->
+@section('active-header')Requests @endsection <!--active pageheader-->
+@section('styles')
+@endsection
+
+@section('content')
+<div class="row">
+    <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+        <div class="card">
+            <div class="card-header" style="display: flex; justify-content: space-between; align-items: center;">
+                <div class="email-title"><span class="icon"><i class="fas fa-hand-holding-usd"></i></span> Claims & Reimbursement <span class="new-messages">(1 new request)</span> </div>
+            </div>
+            <div class="card-body">
+                <div class="table-responsive">
+                    <table class="table table-striped table-bordered first">
+                        <thead>
+                            <tr>
+                                <th>Employee Name</th>
+                                <th>Claim Type</th>
+                                <th>Claim Amount</th>
+                                <th>Description</th>
+                                <th>Date</th>
+                                <th style="width: 0%">Status</th>
+                                <th style="width: 0%">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Art Pregoner </td>
+                                <td>Travel</td>
+                                <td>$320,800</td>
+                                <td>Pamasahe papuntang sm fairview</td>
+                                <td>10-10-2024</td>
+                                <td>Rejected</td>
+                                <td>
+                                    <div class="btn-group ml-auto">
+                                        <button class="btn btn-sm btn-outline-light">Approve</button>
+                                        <button class="btn btn-sm btn-outline-light">
+                                            <i class="far fa-trash-alt"></i>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@section('scripts')
+@endsection

583c3f2da2c493580be6dc96ddad4d1fa15c3ee9
artpregoner <ajcoderpregoner34@gmail.com>
2025-01-12 15:46:58 +0800

    Portal: Claims, show, modal. CSS modal

--- resources/views/portal/claims/show.blade.php
+++ resources/views/portal/claims/show.blade.php
@@ -6,42 +6,55 @@
         <div class="modal-dialog" role="document">
             <div class="modal-content">
                 <div class="card">
-                    <div class="card-header p-4">
-                        {{-- <a class="pt-2 d-inline-block" href="index.html">Concept</a> --}}
+                    <div class="card-header p-2">
+                        <div class="float-left">
+                            <div class="email-title"><span class="icon"><i class="fas fa-hand-holding-usd"></i></span> Expense Claim</div>
+                        </div>
                         <div class="float-right">
-                            <h3 class="mb-0">Invoice #1</h3>
-                            <span>Date: 3 Dec, 2020</span>
+                            <h3 class="mb-0"><span class="badge badge-success">Approve</span></h3>
+                            <span>by: cute</span>
                         </div>
                     </div>
                     <div class="card-body">
                         <div class="row mb-4">
-                            <div class="col-sm-6">
-                                <h5 class="mb-3">From:</h5>
-                                <h3 class="text-dark mb-1">Gerald A. Garcia</h3>
-                                <div>2546 Penn Street</div>
-                                <div>Sikeston, MO 63801</div>
-                                <div>Email: info@gerald.com.pl</div>
-                                <div>Phone: +573-282-9117</div>
-                            </div>
-                            <div class="col-sm-6">
-                                <h5 class="mb-3">To:</h5>
-                                <h3 class="text-dark mb-1">Anthony K. Friel</h3>
-                                <div>478 Collins Avenue</div>
-                                <div>Canal Winchester, OH 43110</div>
-                                <div>Email: info@anthonyk.com</div>
-                                <div>Phone: +614-837-8483</div>
+                            <div class="card-body">
+                                <form action="#" id="basicform" data-parsley-validate="">
+                                    <div class="form-group col-sm-6 pb-2 pb-sm-4 pb-lg-0 pr-0">
+                                        <label for="inputClaimTo">Send Claim To</label>
+                                        {{-- <input id="inputClaimTo" type="text" name="name" data-parsley-trigger="change" required="" placeholder="Enter user name" autocomplete="off" class="form-control"> --}}
+                                        <div class="alert alert-secondary" role="alert">
+                                            Admin, HR
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-sm-2 pb-2 pb-sm-4 pb-lg-0 pr-0">
+                                        <label for="inputClaimDate">Expense Claim Date</label>
+                                        <div class="alert alert-secondary" role="alert">
+                                            06/29/2023
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <label for="inputDescription" class="col-form-label">Description</label>
+                                        <div class="alert alert-secondary" role="alert">
+                                            This is a secondary alert—check it out!
+                                        </div>
+                                    </div>
+                                    <div class="form-group col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
+                                        <label for="inputComments">Comments (Optional)</label>
+                                        <div class="alert alert-secondary" role="alert">
+                                            Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.
+                                        </div>
+                                    </div>
+                                </form>
                             </div>
                         </div>
                         <div class="table-responsive-sm">
                             <table class="table table-striped">
                                 <thead>
                                     <tr>
-                                        <th class="center">#</th>
-                                        <th>Item</th>
-                                        <th>Description</th>
-                                        <th class="right">Unit Cost</th>
-                                        <th class="center">Qty</th>
-                                        <th class="right">Total</th>
+                                        <th class="center">ID</th>
+                                        <th>Category</th>
+                                        <th>Details</th>
+                                        <th class="right">Amount</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -50,61 +63,21 @@
                                         <td class="left strong">Origin License</td>
                                         <td class="left">Extended License</td>
                                         <td class="right">$1,500.00</td>
-                                        <td class="center">1</td>
-                                        <td class="right">$1,500.00</td>
                                     </tr>
                                     <tr>
                                         <td class="center">2</td>
                                         <td class="left">Custom Services</td>
                                         <td class="left">Installation and Customization (cost per hour)</td>
                                         <td class="right">$110.00</td>
-                                        <td class="center">20</td>
-                                        <td class="right">$2,200.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="center">3</td>
-                                        <td class="left">Hosting</td>
-                                        <td class="left">1-year subscription</td>
-                                        <td class="right">$309.00</td>
-                                        <td class="center">1</td>
-                                        <td class="right">$309.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td class="center">4</td>
-                                        <td class="left">Platinum Support</td>
-                                        <td class="left">1-year subscription 24/7</td>
-                                        <td class="right">$5,000.00</td>
-                                        <td class="center">1</td>
-                                        <td class="right">$5,000.00</td>
                                     </tr>
                                 </tbody>
                             </table>
                         </div>
                         <div class="row">
-                            <div class="col-lg-4 col-sm-5"></div>
                             <div class="col-lg-4 col-sm-5 ml-auto">
-                                <table class="table table-clear">
+                                <table class="table table-striped">
                                     <tbody>
-                                        <tr>
-                                            <td class="left">
-                                                <strong class="text-dark">Subtotal</strong>
-                                            </td>
-                                            <td class="right">$9,009.00</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="left">
-                                                <strong class="text-dark">Discount (20%)</strong>
-                                            </td>
-                                            <td class="right">-$1,801.80</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="left">
-                                                <strong class="text-dark">VAT (10%)</strong>
-                                            </td>
-                                            <td class="right">$720.92</td>
-                                        </tr>
-                                        <tr>
-                                            <td class="left">
+                                            <td class="center">
                                                 <strong class="text-dark">Total</strong>
                                             </td>
                                             <td class="right">
@@ -116,8 +89,9 @@
                             </div>
                         </div>
                     </div>
-                    <div class="card-footer bg-white">
-                        <p class="mb-0">2983 Glenview Drive, Corpus Christi, TX 78476</p>
+                    <div class="modal-footer">
+                        <a href="#" class="btn btn-secondary" data-dismiss="modal">Close</a>
+                        <a href="#" class="btn btn-primary">Download</a>
                     </div>
                 </div>
             </div>

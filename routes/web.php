7b5bb97e7f52c51f5e9dbcc64b4b9da8566acf19
artpregoner <ajcoderpregoner34@gmail.com>
2025-02-01 22:46:17 +0800

    storage link

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Authcontroller;
 
+// Route::get('/storage-link', function () {
+//     Artisan::call('storage:link');
+// });
 
 // Authentication routes
 require __DIR__ . '/auth.php';
